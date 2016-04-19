4a7e2922592814072bcdd56782784bc83d0de532
tenkdarko <kwame@tenkorangs-MacBook-Pro.local>
2016-04-19 11:26:32 -0400

    lab10
    lab 10

---
+++ lab10.sql
@@ -0,0 +1,57 @@
+
+create or replace function  PreReqsFor(int, refcursor) returns refcursor as 
+$$
+declare
+   course_num int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+
+	Select * 
+From courses c
+Where c.num in (
+	Select prereqnum 
+	From prerequisites p
+	Where p.coursenum = course_num ) ;
+
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(499, 'results');
+fetch all from results;
+
+
+
+
+
+
+
+---
+
+
+
+create or replace function  ImPreReqsFor(int, refcursor) returns refcursor as 
+$$
+declare
+   course_num int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+
+	Select * 
+From courses c
+Where c.num in (
+	Select coursenum 
+	From prerequisites p
+	Where p.prereqnum = course_num ) ;
+
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select ImPreReqsFor(221, 'results');
+fetch all from results;
+      nh5rti
